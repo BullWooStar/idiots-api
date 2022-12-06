8f063f67b5e4b044923db0b8231c8de37c900b0e
BullWooStar <ccatal66@naver.com>
2022-12-06 13:39:02 +0900

    수정3

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("A18091809aa"),
+    keys: env.array("APP_KEYS"),
   },
 });
